dab76a56e4c4fd27ce6f01e2ea094d301c901f18
JingLong-ZJU <L_J1996@163.com>
2019-09-23 18:44:09 +0800

    lab3 test

---
+++ lab3/Arithmetic/Arithmetic.java
@@ -0,0 +1,21 @@
+
+public class Arithmetic {
+
+	    /** Computes product of two ints */
+
+	    public static int product(int a, int b) {
+	        return a * b;
+	    }
+
+	    /** Computes sum of two ints (incorrectly) */
+
+	    public static int sum(int a, int b) {
+	        return a + b;
+	    }  
+	    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(sum(2,3));
+	}
+
+}
