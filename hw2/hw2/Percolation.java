9e049ffc166eedd8623e942675ea1bfa84f00497
JingLong-ZJU <L_J1996@163.com>
2019-09-25 17:57:12 +0800

    hw2 finish1.0

---
+++ hw2/hw2/Percolation.java
@@ -0,0 +1,84 @@
+package hw2;
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+	private int[][] chess;
+	private int sideLength;
+	private int countOpen=0;
+	WeightedQuickUnionUF uf;
+	WeightedQuickUnionUF uf_top;
+	private int top;
+	private int bottom;	
+	private int[][] neighborDirection = {{0,1},{1,0},{-1,0},{0,-1}};
+	public Percolation(int N) {
+		// create N-by-N grid, with all sites initially blocked
+		if(N<=0) {
+			throw new IllegalArgumentException(); 
+		}
+		sideLength=N;
+		uf = new WeightedQuickUnionUF(sideLength*sideLength+2);//the biggest two is for top and bottom
+		uf_top= new WeightedQuickUnionUF(sideLength*sideLength+1);//only for isFull method
+		top=sideLength*sideLength;     //top relation
+		bottom=sideLength*sideLength+1;
+		chess = new int[N][N];	
+		for(int i=0;i<N;i++) {
+			for(int j=0;i<N;i++) {
+				chess[i][j]=0;
+			}
+		}		
+	}
+	
+	private int getLoc(int row,int col) {
+		validateLoc(row,col);
+		return row*sideLength+col;
+	}
+	
+	private void validateLoc(int row,int col) {
+		if(row>=sideLength||row<0||col>=sideLength||col<0) {
+			throw new IndexOutOfBoundsException();
+		}
+	}
+	
+	public void open(int row, int col) {
+		// open the site (row, col) if it is not open already
+		validateLoc(row,col);
+		if(!isOpen(row,col)) {
+			chess[row][col] = 1;
+			countOpen++;	
+			if(row == 0) {
+				uf.union(top, getLoc(row, col));
+				uf_top.union(top,getLoc(row, col));
+			}
+			if (row == sideLength-1) {
+				uf.union(bottom, getLoc(row, col));
+			}
+			for(int[]d : neighborDirection) {
+				int neighborRow = row+d[0];
+				int neighborCol = col+d[1];
+				if(neighborRow>=0&&neighborRow<sideLength&&neighborCol>=0&&neighborCol<sideLength&&isOpen(neighborRow, neighborCol)) {
+					uf.union(getLoc(row, col), getLoc(neighborRow, neighborCol));
+					uf_top.union(getLoc(row, col), getLoc(neighborRow, neighborCol));
+				}
+			}
+		}
+	}
+	public boolean isOpen(int row, int col) {
+		// is the site (row, col) open?
+		validateLoc(row,col);
+		return chess[row][col] == 1;
+	}
+	public boolean isFull(int row, int col) {
+		// is the site (row, col) full?
+		validateLoc(row,col);
+		return uf_top.connected(top, getLoc(row,col));
+	}
+	public int numberOfOpenSites() {
+		// number of open sites
+		return countOpen;
+	}
+	public boolean percolates() {
+		// does the system percolate?
+		return countOpen>0&&uf.connected(top, bottom);
+	}
+}
---
+++ hw2/hw2/PercolationStats.java
@@ -0,0 +1,48 @@
+package hw2;
+
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
+public class PercolationStats {
+	private double[] ratio;	//threshold
+	private Percolation exp;
+	private int times; 
+	public PercolationStats(int N, int T, PercolationFactory pf) {
+		// perform T independent experiments on an N-by-N grid
+		if(N<=0||T<=0) {
+			throw new IllegalArgumentException();
+		}
+		ratio = new double[T];
+		this.times=T;
+		for(int i = 0;i<T;i++) {
+			exp = pf.make(N);
+			int row,col;
+			while(!exp.percolates()) {
+				row = StdRandom.uniform(N);
+				col = StdRandom.uniform(N);
+				exp.open(row, col);				
+			}
+			if(exp.percolates()) {
+				ratio[i]=(double) exp.numberOfOpenSites()/N;
+			}
+		}
+	}
+	public double mean() {
+		// sample mean of percolation threshold
+		return StdStats.mean(ratio);
+	}
+	public double stddev() {
+		// sample standard deviation of percolation threshold
+		if(this.times == 1)
+			return Double.NaN;
+		return StdStats.stddev(ratio);	
+	}
+	public double confidenceLow() {
+		// low endpoint of 95% confidence interval
+		return StdStats.mean(ratio)-1.96*StdStats.stddev(ratio)/Math.sqrt(times);
+	}
+	public double confidenceHigh() {
+		// high endpoint of 95% confidence interval
+		return StdStats.mean(ratio)+1.96*StdStats.stddev(ratio)/Math.sqrt(times);	
+	}
+}
