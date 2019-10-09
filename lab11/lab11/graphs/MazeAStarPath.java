e5dfe33d376cd4fcf744184f18d4def36ebc4732
JingLong-ZJU <L_J1996@163.com>
2019-10-09 22:04:57 +0800

    lab11 finish1.2

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import java.util.Comparator;
+
+import edu.princeton.cs.algs4.MinPQ;
+
 /**
  *  @author Josh Hug
  */
@@ -8,6 +12,7 @@ public class MazeAStarPath extends MazeExplorer {
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+    MinPQ<Integer> pq;
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -16,28 +21,62 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        pq = new MinPQ<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if(h(o1) + distTo[o1] < h(o2) + distTo[o2])
+					return -1;
+				else if(h(o1) + distTo[o1] > h(o2) + distTo[o2])
+					return 1;
+				else return 0;
+			}
+		});
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+    	int sourceX = maze.toX(s);
+    	int sourceY = maze.toY(s);
+    	int targetX = maze.toX(t);
+    	int targetY = maze.toY(t);
+    	return Math.abs(sourceX - targetX) + Math.abs(sourceY - targetY);
     }
 
     /** Finds vertex estimated to be closest to target. */
     private int findMinimumUnmarked() {
-        return -1;
-        /* You do not have to use this method. */
+        return pq.delMin();
     }
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+    	pq.insert(s);
+        marked[s] = true;
+        announce();
+        while(!pq.isEmpty()){
+        	int v = findMinimumUnmarked();
+        	if (v == t) {
+                targetFound = true;
+            }
+            if (targetFound) {
+                return;
+            }
+            for(int w : maze.adj(v)) {
+            	if(!marked[w]) {
+            		pq.insert(w);
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    announce();
+            	}
+            }
+            
+            
+        }
     }
 
     @Override
     public void solve() {
         astar(s);
     }
-
 }
 
